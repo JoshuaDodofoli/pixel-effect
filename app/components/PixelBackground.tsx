93ac4dae0af45ebfd946982720b3e6464ec4abe5
JoshuaDodofoli <dodofolijoshua@gmail.com>
2026-03-26 21:45:40 +0000

    Created blocks.

---
+++ app/components/PixelBackground.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
+
+const PixelBackground = () => {
+    const [numberOfBlocks, setNumberOfBlocks] = useState(0);
+    const block = useRef<(HTMLDivElement | null)[]>([]);
+
+    useEffect(() => {
+        const { innerWidth, innerHeight } = window;
+        const blockSize = innerWidth * 0.05;
+        setNumberOfBlocks(Math.floor(innerHeight / blockSize));
+    }, []);
+
+    useGSAP(() => {
+        gsap.set(block.current, { backgroundColor: "transparent" });
+    }, [numberOfBlocks]);
+
+    const handleMouseEnter = (index: number) => {
+        gsap.to(block.current[index], { duration: 0.1, backgroundColor: "#3b82f6" });
+    };
+
+    const handleMouseLeave = (index: number) => {
+        gsap.to(block.current[index], { duration: 0.1, backgroundColor: "transparent" });
+    };
+
+    const createBlocks = (colIndex: number) => {
+        return [...Array(numberOfBlocks)].map((_, i) => {
+            const index = colIndex * numberOfBlocks + i;
+            return (
+                <div
+                    key={i}
+                    className="w-full h-[5vw] bg-blue-500"
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                    ref={(el) => { block.current[index] = el }}
+                />
+            )
+        });
+    }
+
+    return (
+        <div className='w-full flex h-full'>
+            {[...Array(20)].map((_, i) => (
+                <div key={i} className="w-[5vw] h-full">
+                    {createBlocks(i)}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default PixelBackground
